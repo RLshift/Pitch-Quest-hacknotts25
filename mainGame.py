fe43328e74896cbb83fd596ee078f2f50047717a
Opixior <sfungworkuk@gmail.com>
2025-10-26 00:43:33 +0100

    Flautist Sprite added

--- mainGame.py
+++ mainGame.py
@@ -11,18 +11,22 @@ from pyglet import image
 pic = image.load('backgroundGamePlay.png')
 width, height = pic.width, pic.height
 
+flautist = image.load('flautist.png')
+sprite = pyglet.sprite.Sprite(img=flautist, x=160, y=-25)
+#width, height = flautist.width, flautist.height
 # Making Green Circle
 circle = shapes.Circle(700, 150, 100, 
                        color=(50, 225, 30),
                        batch=batch)
 
 
-
 @window.event
 def on_draw():
     window.clear()
     pic.blit(0, 0, 0)
-    batch.draw()
+    #flautist.blit(200,200,200)
+    sprite.draw()
+    #batch.draw()
 
 
 pyglet.app.run()
