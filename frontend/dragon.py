f8f9f0199c56227818a3349281d2eb94ffb37857
psypl5 <psypl5@nottingham.ac.uk>
2025-10-26 05:00:04 +0000

    dragon.py

---
+++ frontend/dragon.py
@@ -0,0 +1,14 @@
+class Dragon: 
+    def __init__(self):
+        self.health = 100
+        self.alive = True
+    
+    def getHealth(self):
+        return self.health
+    
+    def redHealth(self, reduce):
+        self.health -= reduce
+    
+    def checkAlive(self):
+        if self.health == 0:
+            self.alive = False
--- frontend/player.py
+++ frontend/player.py
@@ -1,5 +1,3 @@
-import random
-
 class Player: 
 
     def __init__(self):
